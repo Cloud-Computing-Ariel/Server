7449e1b5aebe87fba8eb2a198eaa434b9077555f
Alex Baranov <73061540+SaltyByte@users.noreply.github.com>
2023-03-10 15:17:15 +0200

    added dashboard date update

--- server/src/api/v1/dashboard/dashboard.service.ts
+++ server/src/api/v1/dashboard/dashboard.service.ts
@@ -1,4 +1,15 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class DashboardService {}
+export class DashboardService {
+  constructor(private readonly httpService: HttpService) {}
+
+  getHeaderCards() {
+    this.httpService.get('http://localhost:3001/get-header');
+  }
+
+  getBodyCards() {
+    this.httpService.get('http://localhost:3001/get-body');
+  }
+}
